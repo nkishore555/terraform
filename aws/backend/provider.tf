2e209cf7239816d1c650d70edf95bce7ac9b662b
nkishore555 <143krrish143@gmail.com>
2023-03-29 13:20:01 +0530

    added backend file

--- aws/backend/provider.tf
+++ aws/backend/provider.tf
@@ -5,8 +5,16 @@ terraform {
       version = "4.60.0"
     }
   }
+  required_version = "> 1.0.0"
+  backend "s3" {
+    bucket         = "myterraform321"
+    key            = "terraform/mylockfile"
+    dynamodb_table = "mytable"
+    region         = "ap-south-1"
+
+  }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  # Configuration options
 }
