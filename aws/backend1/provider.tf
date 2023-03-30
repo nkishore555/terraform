73c2251763a7b870f0c42af2f2b347f599e47543
nkishore555 <143krrish143@gmail.com>
2023-03-30 12:41:34 +0530

    hello

--- aws/backend1/provider.tf
+++ aws/backend1/provider.tf
@@ -6,6 +6,15 @@ terraform {
     }
   }
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "kishore"
+    storage_account_name = "kishore987"
+    container_name       = "mycont"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
